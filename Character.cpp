073140893944036673932b15142778497833ec58
Madison <madison.oneill@student.northampton.edu>
2026-04-15 12:37:52 -0400

    define virtual functions that will be used in all derived classes (so far all health related) - health getter and setter - maxHealth getter and setter - take damage

--- Character.cpp
+++ Character.cpp
@@ -1,1 +1,21 @@
 #include "Character.h"
+
+int Character::getHealth() {
+    return currentHealth;
+}
+
+void Character::setHealth(const int currentHealth) {
+    this->currentHealth = currentHealth;
+}
+
+int Character::getMaxHealth() {
+    return maxHealth;
+}
+
+void Character::setMaxHealth(const int maxHealth) {
+    this->maxHealth = maxHealth;
+}
+
+int Character::takeDamage(const int damage) {
+    return currentHealth -= damage;
+}
--- Character.h
+++ Character.h
@@ -1,9 +1,28 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-
+// reminder that a pure virtual class will have different behaviors for the same function in derrived classes
+// common behaviors can be defined in .cpp as regular functions within this class (?)
 class Character {
+public:
+    // TODO - define different behavior in derived classes
+    virtual void attack() = 0;
+    virtual void defense() = 0;
 
+    virtual int takeDamage(int);
+
+    // public getters & protected setters
+    virtual int getHealth();
+    virtual int getMaxHealth();
+
+protected:
+    virtual void setHealth(int);
+    virtual void setMaxHealth(int);
+
+private:
+    // TODO - can this health int be private? how would derived classes interact with it?
+    int currentHealth = 100;
+    int maxHealth = 100;
 };
 
 
